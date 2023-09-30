664575e5bfc6ec2cce1d114f4977ec1acf439fbd
caden-miller <cadenmiller40@gmail.com>
2023-09-30 17:36:39 -0500

    fixed logos for experience section so that they are now uniform and display the proper colors

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -32,7 +32,7 @@ const Projects = () => {
   ]
 
   return (
-    <div name='projects' className='bg-gradient-to-b from-gray-800 to-black w-full text-white md:h-screen'>
+    <div name='projects' className='flex bg-gradient-to-b from-gray-800 to-black w-full text-white md:h-screen'>
       
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
 
@@ -43,13 +43,14 @@ const Projects = () => {
 
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
           {
-            projects.map(({id, src}) => (
+            projects.map(({id, src, codeLink}) => (
               
                 <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                   <img src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
                   <div className='flex items-center justify-center '>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                    <a href={codeLink} target='_blank' rel='noopener noreferrer'>
+                      <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                    </a>
                   </div>
                 </div>
               
